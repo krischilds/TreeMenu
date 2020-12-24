7b4ae89bd65fbdb4ddf986bf4127778e08c3511c
Kris Childs <kris.childs@gmail.com>
2020-12-24 12:58:59 -0800

    moved comp files to new folder

---
+++ src/components/General/CardLayout.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+
+    <ul class="grid-container">
+      <li class="title">{{ title }}</li>
+      <li class="block" v-for="item in items" :key="item.id" :class="item.abbr">    
+        <slot v-bind="item">{{item.name}}</slot>
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardLayout",
+  props: [ 'title', 'items' ],
+};
+</script>
+
+
